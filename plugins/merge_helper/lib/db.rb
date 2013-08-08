4f4b7491df1566cb0804f02c9da720d2b9da2550
Tom Christ <tchrist@tumblr.com>
2013-08-08 14:26:40 -0400

    Change aggregation flag check logic

--- plugins/merge_helper/lib/db.rb
+++ plugins/merge_helper/lib/db.rb
@@ -2,8 +2,8 @@ module Jetpants
   class DB
     def aggregator?
       return @aggregator unless @aggregator.nil?
-      version_info = query_return_array('SHOW VARIABLES LIKE "%version%"')
-      @aggregator = !version_info[:version_comment].nil? && version_info[:version_comment].downcase.include?("mariadb")
+      version_info = query_return_array('SHOW VARIABLES LIKE "version"')
+      @aggregator = !version_info.nil? && !version_info.empty? && version_info.first[:Value].downcase.include?("mariadb")
     end
 
   end
