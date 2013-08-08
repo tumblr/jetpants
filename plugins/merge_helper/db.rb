9b6fd05c13dbd88d9a29aac854fd4104f58ad815
Tom Christ <tchrist@tumblr.com>
2013-08-08 13:30:38 -0400

    Adding sanity check in aggregator class constructor

--- plugins/merge_helper/db.rb
+++ plugins/merge_helper/db.rb
@@ -1,5 +1,10 @@
 module Jetpants
   class DB
+    def initialize(ip, port=3306)
+      raise "Attempting to initialize a database without aggregation capabilities as an aggregate node" unless aggregate?
+
+      super
+    end
 
     def aggregator?
       return @aggregator unless @aggregator.nil?
