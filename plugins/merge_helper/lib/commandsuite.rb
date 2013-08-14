07f5f7063593582612e993dec2446bfdef839fd3
Tom Christ <tchrist@tumblr.com>
2013-08-14 15:16:09 -0400

    Modularize logic for building up aggregate node and shipping aggregate data to new master

--- plugins/merge_helper/lib/commandsuite.rb
+++ plugins/merge_helper/lib/commandsuite.rb
@@ -13,84 +13,7 @@ module Jetpants
       aggregate_shard = Shard.new(shards_to_merge.first.min_id, shards_to_merge.last.max_id, aggregate_node, :initializing)
       Jetpants.topology.pools << aggregate_shard 
 
-      total_export_counts = {}
-      total_import_counts = {}
-      slaves_to_replicate = []
-
-      # settings to improve import speed
-      aggregate_node.restart_mysql '--skip-log-bin', '--skip-log-slave-updates', '--innodb-autoinc-lock-mode=2', '--skip-slave-start'
-      tables = Table.from_config 'sharded_tables'
-
-      # create and ship schema
-      slave = shards_to_merge.last.standby_slaves.last
-      slave.ship_schema_to aggregate_node
-      aggregate_node.import_schemata!
-
-      # grab slave list to export data
-      slaves_to_replicate = shards_to_merge.map { |shard| shard.standby_slaves.last }
-
-      # asynchronously export data on all slaves
-      slaves_to_replicate.concurrent_each do |slave|
-        # these get cleaned up further down after replication is set up
-        slave.disable_monitoring
-        slave.stop_query_killer
-        slave.pause_replication
-
-        export_counts = slave.export_data tables, slave.pool.min_id, slave.pool.max_id
-
-        if total_export_counts.empty?
-          total_export_counts = export_counts
-        else
-          total_export_counts.keys.each do |key|
-            total_export_counts[key] = total_export_counts[key] + export_counts[key]
-          end
-        end
-      end
-
-      # iterate through and load data from each slave
-      slaves_to_repliate.each do |slave|
-        
-        slave.fast_copy_chain(
-          Jetpants.export_location,
-          aggregate_node,
-          port: 3307,
-          files: slave.pool.table_export_filenames(full_path = false),
-          overwrite: true
-        )
-
-        import_counts = aggregate_node.import_data tables, slave.pool.min_id, slave.pool.max_id
-        if total_import_counts.empty?
-          total_import_counts = import_counts
-        else
-          total_import_counts.keys.each do |key|
-            total_import_counts[key] = total_import_counts[key] + import_counts[key]
-          end
-        end
-      end
-
-      # clear out earlier import options
-      aggregate_node.restart_mysql
-
-      # validate import counts
-      raise "Imported and exported table count doesn't match!" unless total_import_counts.keys.count == total_export_counts.keys.count
-      valid = true;
-      total_import_count.each do |key, val|
-        if val != total_export_count[key]
-          output "Count for export/import of #{key} is wrong! (#{val} imported #{total_export_count[key]} exported)"
-          valid = false
-        end
-      end
-
-      raise "Import/export counts do not match, aborting" unless valid
-
-      # resume replication on source nodes and catch up to master
-      aggregate_node.add_nodes_to_aggregate slaves_to_replicate
-      slaves_to_replicate.concurrent_each do |slave|
-        slave.resume_replication
-        slave.catch_up_to_master
-        slave.enable_monitoring
-        slave.start_query_killer
-      end
+      aggregate_node = Shard.set_up_aggregate_node(shards_to_merge, aggregate_node_id)
 
       aggregate_node.start_all_slaves
 
@@ -105,45 +28,11 @@ module Jetpants
       spares_for_aggregate_shard = Jetpants.topology.claim_spares(Jetpants.standby_slaves_per_pool + 1, role: :standby_slave, like: shards_to_merge.first.master)
       aggregate_shard_master = spares_for_aggregate_shard.pop
 
-      # export and ship schema to new aggregated shard master
-      aggregate_node.export_schemata tables
-      aggregate_node.ship_schema_to aggregate_shard_master
-      aggregate_shard_master.import_schemata!
-
-      # export and ship data to new shard master
-      aggregate_node.stop_all_replication
-      aggregate_export_counts = aggregate_node.export_data tables, shards_to_merge.first.min_id, shards_to_merge.last.max_id
-      aggregate_node.fast_copy_chain(
-        Jetpants.export_location,
-        aggregate_shard_master,
-        port: 3307,
-        files: aggregate_shard.table_export_filenames(full_path = false),
-        overwrite: true
-      )
-      aggregate_shard_master.restart_mysql '--skip-log-bin', '--skip-log-slave-updates', '--innodb-autoinc-lock-mode=2', '--skip-slave-start'
-      aggregate_import_counts = aggregate_shard_master.import_data tables, shards_to_merge.first.min_id, shards_to_merge.last.max_id
-      aggregate_shard_master.restart_mysql
-
-      # validate counts from load_data_infile / select_into_outfile
-      valid_import = true
-      aggregate_export_counts.each do |key, count|
-        if count != aggregate_import_counts[key]
-          output "Count for aggregate export/import of #{key} is wrong! (#{aggregate_import_counts[key]} imported #{count} exported)"
-          valid_import = false
-        end
-      end
-      raise "Import/export counts do not match, aborting" unless valid_import
-
-      aggregate_shard_master.change_master_to aggregate_node
-      aggregate_node.start_all_slaves
-      # catch aggregate node up to data sources
-      slaves_to_replicate.each do |shard_slave|
-        aggregate_node.aggregate_catch_up_to_master shard_slave
-      end
-      aggregate_shard_master.catch_up_to_master
+      Shard.ship_aggregate_data_to_new_master(aggregate_node, new_shard_master)
 
       # build up the rest of the new shard
       aggregate_shard_master.enslave! spares_for_aggregate_shard
+      aggregate_shard.master = aggregate_shard_master
 
       sync_configuration
     end
