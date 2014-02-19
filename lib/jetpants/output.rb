119769b9a421c55c4e0e789d4f675ef35658b26f
Tyler Neely <tan@tumblr.com>
2014-02-19 14:36:09 -0500

    simplify implementation

--- lib/jetpants/output.rb
+++ lib/jetpants/output.rb
@@ -3,20 +3,19 @@ module Jetpants
   # This mixin defines an output function that prints the class, and optionally
   # caller file, method and line number if output_caller_info is set.
   module Output
-    def self.included(base)
-      base.class_eval do
-        def output str, table=nil
-          str = str.to_s.strip
-          str = nil if str && str.length == 0
-          str ||= "Completed (no output)"
-          output = Time.now.strftime("%H:%M:%S") + " [#{self}] "
-          output << "called from #{caller[0]} " if Jetpants.output_caller_info
-          output << table.name << ': ' if table
-          output << str
-          print output + "\n"
-          output
-        end
+    def output str, table=nil
+      str = if str.is_a? String and str.length == 0
+        "Completed (no output)"
+      else
+        str.to_s.strip
       end
+
+      output = Time.now.strftime("%H:%M:%S") + " [#{self}] "
+      output << "called from #{caller[0]} " if Jetpants.output_caller_info
+      output << table.name << ': ' if table
+      output << str
+      print output + "\n"
+      output
     end
   end
 end
