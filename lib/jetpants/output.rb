f3d6809168d75faf2f33f3da3f5fcdf636e60376
Kiril Angov <kiril.angov@gmail.com>
2014-11-20 11:56:15 -0500

    Make sure we do the proper locking around the Jetpants::output method

--- lib/jetpants/output.rb
+++ lib/jetpants/output.rb
@@ -2,10 +2,15 @@ require 'logger'
 
 module Jetpants
 
+  attr_accessor :output_lock
+  attr_accessor :output_logger
+
   # This mixin defines an output function that prints the class, and optionally
   # caller file, method and line number if output_caller_info is set.
   module Output
     def output(str = "\s", table = nil, level = :info)
+      @output_lock = @output_lock || Mutex.new
+
       str = if str.nil? or (str.is_a? String and str.length == 0)
               "Completed (no output)"
             else
@@ -18,18 +23,22 @@ module Jetpants
       output << table.name << ': ' if table
       output << str
 
-      context = self.to_s == 'console' ? 'console' : self.class.name
-      logger = Logger.new(Jetpants.log_file, 'daily')
-      logger.send(level, context) {
-        output
-          .gsub(/\e\[\d+(;\d+)*m/, '') # remove all coloring from Highline
-          .gsub(/^#{level}: /i, '')    # remove repetition of the logging level
-      }
+      unless Jetpants.log_file.to_s.empty?
+        context = self.to_s == 'console' ? 'console' : self.class.name
+        @output_logger = @output_logger || Logger.new(Jetpants.log_file)
+        @output_logger.send(level, context) {
+          output
+            .gsub(/\e\[\d+(;\d+)*m/, '') # remove all coloring from Highline
+            .gsub(/^#{level}: /i, '')    # remove repetition of the logging level
+        }
+      end
 
       # add the current time for display purposes
       output = [Time.now.strftime("%H:%M:%S"), output].join(' ')
+      @output_lock.synchronize {
+        puts output
+      }
 
-      puts output
       output
     end
   end
