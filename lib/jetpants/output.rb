f5b16965e88f81f94a23683d26072257461b190a
Tom Christ <tchrist@tumblr.com>
2014-11-20 18:42:21 -0500

    Use class variables

--- lib/jetpants/output.rb
+++ lib/jetpants/output.rb
@@ -8,6 +8,9 @@ module Jetpants
   # This mixin defines an output function that prints the class, and optionally
   # caller file, method and line number if output_caller_info is set.
   module Output
+    @@output_lock = nil
+    @@output_logger = nil
+
     def output(str = "\s", table = nil, level = :info)
       str = if str.nil? or (str.is_a? String and str.length == 0)
               "Completed (no output)"
@@ -23,8 +26,8 @@ module Jetpants
 
       unless Jetpants.log_file.to_s.empty?
         context = self.to_s == 'console' ? 'console' : self.class.name
-        @output_logger ||= Logger.new(Jetpants.log_file)
-        @output_logger.send(level, context) {
+        @@output_logger ||= Logger.new(Jetpants.log_file)
+        @@output_logger.send(level, context) {
           output
             .gsub(/\e\[\d+(;\d+)*m/, '') # remove all coloring from Highline
             .gsub(/^#{level}: /i, '')    # remove repetition of the logging level
@@ -33,7 +36,7 @@ module Jetpants
 
       # add the current time for display purposes
       output = [Time.now.strftime("%H:%M:%S"), output].join(' ')
-      (@output_lock ||= Mutex.new).synchronize {
+      (@@output_lock ||= Mutex.new).synchronize {
         puts output
       }
 
