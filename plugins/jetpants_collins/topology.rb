87f5b91cd2088d0a97bf361179b119b793d2bd5f
Tom Christ <tchrist@tumblr.com>
2015-02-11 15:55:58 -0500

    Verify that source has a pool when comparing

--- plugins/jetpants_collins/topology.rb
+++ plugins/jetpants_collins/topology.rb
@@ -296,7 +296,7 @@ module Jetpants
         if(db.usable_spare? &&
           (
             !source ||
-            db.usable_with?(source) ||
+            (!source.pool && db.usable_with?(source)) ||
             (source.pool && db.usable_in?(source.pool))
           )
         )
