9a4bff42638be7ba7f296a1337ecfdcfde254b02
Tom Christ <tchrist@tumblr.com>
2015-08-31 16:45:56 -0400

    Add logic to conditionally encrypt based on target(s)

--- lib/jetpants/host.rb
+++ lib/jetpants/host.rb
@@ -215,10 +215,17 @@ module Jetpants
         output "Compression disabled -- no compression method specified in Jetpants config file"
       end
 
-      if Jetpants.encrypt_with && Jetpants.decrypt_with
+      should_encrypt = false
+      targets.each do |t|
+        should_encrypt = should_encrypt || should_encrypt_with? t
+      end
+
+      if Jetpants.encrypt_with && Jetpants.decrypt_with && should_encrypt
         enc_bin = Jetpants.encrypt_with.split(' ')[0]
         confirm_installed enc_bin
         output "Using #{enc_bin} for encryption"
+      else
+        output "Not encrypting data stream, either no encryption method specified or encryption unneeded with target"
       end
       
       # On each destination host, do any initial setup (and optional validation/erasing),
@@ -235,7 +242,7 @@ module Jetpants
           t.confirm_installed decomp_bin
         end
 
-        if Jetpants.encrypt_with && Jetpants.decrypt_with
+        if Jetpants.encrypt_with && Jetpants.decrypt_with && should_encrypt
           decrypt_bin = Jetpants.decrypt_with.split(' ')[0]
           t.confirm_installed decrypt_bin
         end
@@ -251,7 +258,7 @@ module Jetpants
         end
         
         decompression_pipe = Jetpants.decompress_with ? "| #{Jetpants.decompress_with}" : ''
-        decryption_pipe = Jetpants.decrypt_with ? "| #{Jetpants.decrypt_with}" : ''
+        decryption_pipe = (Jetpants.decrypt_with && should_encrypt) ? "| #{Jetpants.decrypt_with}" : ''
         if i == 0
           workers << Thread.new { t.ssh_cmd "cd #{dir} && nc -l #{port} #{decryption_pipe} #{decompression_pipe} | tar xv" }
           t.confirm_listening_on_port port
@@ -272,7 +279,7 @@ module Jetpants
       # Start the copy chain.
       output "Sending files over to #{targets[0]}: #{file_list}"
       compression_pipe = Jetpants.compress_with ? "| #{Jetpants.compress_with}" : ''
-      encryption_pipe = Jetpants.encrypt_with ? "| #{Jetpants.encrypt_with}" : ''
+      encryption_pipe = (Jetpants.encrypt_with && should_encrypt) ? "| #{Jetpants.encrypt_with}" : ''
       ssh_cmd "cd #{base_dir} && tar vc #{file_list} #{compression_pipe} #{encryption_pipe} | nc #{targets[0].ip} #{port}"
       workers.each {|th| th.join}
       output "File copy complete."
@@ -283,6 +290,12 @@ module Jetpants
       output "Verifying file sizes and types on all destinations."
       compare_dir base_dir, destinations, options
       output "Verification successful."
+    end
+
+    # Add a hook point to determine whether a host should encrypt a data stream between two hosts
+    # This is useful to avoid encryption latency in a secure environment
+    def should_encrypt_with?(host)
+      true
     end
     
     # Given the name of a directory or single file, returns a hash of filename => size of each file present.
