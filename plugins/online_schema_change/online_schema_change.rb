f824ee2aa5cacdeb1cd72a323a173dfa26f42c05
Graham Christensen <graham@tumblr.com>
2016-10-12 12:29:33 -0400

    Streamline requires

--- plugins/online_schema_change/online_schema_change.rb
+++ plugins/online_schema_change/online_schema_change.rb
@@ -1,10 +1,7 @@
 require 'json'
 
-# load all the monkeypatches for other Jetpants classes
-%w(pool db topology commandsuite).each {|mod| require "online_schema_change/lib/#{mod}"}
-
-require 'online_schema_change/lib/ptosc'
-require 'online_schema_change/lib/percona-dsn'
+# load all the monkeypatches for other Jetpants classes, and our libraries
+%w(pool db topology commandsuite ptosc percona-dsn).each {|mod| require "online_schema_change/lib/#{mod}"}
 
 if Jetpants.plugin_enabled? 'jetpants_collins'
   require 'online_schema_change/lib/collins'
