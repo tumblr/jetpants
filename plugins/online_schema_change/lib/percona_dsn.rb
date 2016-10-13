e0182343d7735dc30eaf8f6f6da217474ff0c167
Graham Christensen <graham@tumblr.com>
2016-10-13 09:49:27 -0400

    remove debugging output

--- plugins/online_schema_change/lib/percona_dsn.rb
+++ plugins/online_schema_change/lib/percona_dsn.rb
@@ -26,19 +26,14 @@ module Jetpants
       end
 
       def create_table
-        puts "pre"
         @server.mysql_root_cmd "SET SESSION sql_log_bin = 0; CREATE DATABASE IF NOT EXISTS #{SCHEMA_NAME}"
-        puts "mid"
-        puts schema
         @server.mysql_root_cmd "SET SESSION sql_log_bin = 0; #{schema}"
-        puts "post"
       end
 
       def load_nodes nodes
         if nodes.length > 0
           values = nodes.map { |node| "('h=#{node.ip}')" }.join(', ')
           myquery = "INSERT INTO #{SCHEMA_NAME}.#{TABLE_NAME} (dsn) VALUES #{values}"
-          puts "QUERY TO RUN: #{myquery}"
           @server.mysql_root_cmd("SET SESSION sql_log_bin = 0; #{myquery}")
         end
       end
