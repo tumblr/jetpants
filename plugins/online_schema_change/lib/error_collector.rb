49eaf08749af40b364c14c594b32cfec23207865
Graham Christensen <graham@tumblr.com>
2016-11-30 11:51:08 -0500

    ptosc: Create and use a helper for the preflight-then-run jobs
    This pattern of running the job on one shard, prompting, then running it
    on all the rest of the shards is repeated many places. This helper and
    UI bit simplifies the topology code by only putting the actual action in
    one place.

---
+++ plugins/online_schema_change/lib/error_collector.rb
@@ -0,0 +1,53 @@
+require 'thread'
+
+module Jetpants
+  # Usage:
+  #
+  # t = Jetpants::ErrorCollector.new
+  # t.run("foo") { 1 }
+  # t.run("bar") { 1 }
+  # t.run("baz") { 3 }
+  # t.run("tux") { raise "whut" }
+  #
+  # pp t.returns
+  # pp t.exception
+
+  class ErrorCollector
+    def initialize
+      @semaphore = Mutex.new
+      @returns = {}
+      @exceptions = {}
+    end
+
+    def run key, &block
+      record_return key, block.call()
+    rescue => exception
+      record_exception key, exception
+      false
+    end
+
+    def record_exception key, e
+      @semaphore.synchronize do
+        @exceptions[key] = e
+      end
+    end
+
+    def record_return key, ret
+      @semaphore.synchronize do
+        @returns[key] = ret
+      end
+    end
+
+    def returns
+      @semaphore.synchronize do
+        return @returns.dup
+      end
+    end
+
+    def exceptions
+      @semaphore.synchronize do
+        return @exceptions.dup
+      end
+    end
+  end
+end
--- plugins/online_schema_change/lib/topology.rb
+++ plugins/online_schema_change/lib/topology.rb
@@ -8,40 +8,16 @@ module Jetpants
     def alter_table_shards(database, table, alter, dry_run=true, shard_pool=nil, skip_rename=false, arbitrary_options=[])
       shard_pool = Jetpants.topology.default_shard_pool if shard_pool.nil?
       my_shards = shards(shard_pool).dup
-      first_shard = my_shards.shift
-      output "Will run on first shard and prompt for going past the dry run only on the first shard\n\n"
-      output "#{first_shard.pool.to_s}\n"
-      unless first_shard.alter_table(database, table, alter, dry_run, false, skip_rename, arbitrary_options)
-        output "First shard had an error, please check output\n"
-        return
+
+
+      ui = PreflightShardUI.new(my_shards)
+      ui.on_each do |shard,stage|
+        # If we're past preflight, we want to not prompt the confirmation.
+        force = stage == :all
+        shard.alter_table(database, table, alter, dry_run, force, skip_rename, arbitrary_options)
       end
 
-      continue = ask('First shard complete would you like to continue with the rest of the shards?: (YES/no) - YES has to be in all caps and fully typed')
-      if continue == 'YES'
-        errors = []
-        exceptions = {}
-
-        my_shards.limited_concurrent_map(10) do |shard|
-          output "#{shard.pool.to_s}\n"
-          begin
-            errors << shard unless shard.alter_table(database, table, alter, dry_run, true, skip_rename, arbitrary_options)
-          rescue Exception => e
-            output "#{shard.pool.to_s} threw exception: #{e}".red
-            errors << shard
-            exceptions[shard.name] << e
-          end
-        end
-
-        errors.each do |shard|
-          output "check #{shard.name} for errors during online schema change\n"
-        end
-
-        exceptions.each do |pool, exception|
-          output "Pool '#{pool}' threw exception: #{exception}".red
-          output "Backtrace:"
-          output e.backtrace.join("\n\t").red
-        end
-      end
+      ui.run!
     end
 
     # will drop old table from the shards after a alter table
@@ -51,36 +27,19 @@ module Jetpants
     def drop_old_alter_table_shards(database, table, shard_pool = nil)
       shard_pool = Jetpants.topology.default_shard_pool if shard_pool.nil?
       my_shards = shards(shard_pool).dup
-      first_shard = my_shards.shift
-      output "Will run on first shard and prompt before going on to the rest\n\n"
-      output "#{first_shard.pool.to_s}\n"
-      first_shard.drop_old_alter_table(database, table)
 
-      continue = ask('First shard complete would you like to continue with the rest of the shards?: (YES/no) - YES has to be in all caps and fully typed')
-      if continue == 'YES'
-        my_shards.each do |shard|
-          print "#{shard.pool.to_s}\n"
-          shard.drop_old_alter_table(database, table)
-        end
-      end
+      ui = PreflightShardUI.new(my_shards)
+      ui.on_each { |shard,_| shard.drop_old_alter_table(database, table) }
+      ui.run!
     end
 
     def rename_table_shards(database, orig_table, copy_table, shard_pool=nil)
       shard_pool = Jetpants.topology.default_shard_pool if shard_pool.nil?
       my_shards = shards(shard_pool).dup
-      first_shard = my_shards.shift
-      output "Will run on first shard and prompt before going on to the rest\n\n"
-      output "#{first_shard.pool.to_s}\n"
-      first_shard.rename_table(database, orig_table, copy_table)
 
-      continue = ask('First shard complete would you like to continue with the rest of the shards?: (YES/no) - YES has to be in all caps and fully typed')
-      if continue == 'YES'
-        my_shards.each do |shard|
-          print "#{shard.pool.to_s}\n"
-          shard.rename_table(database, orig_table, copy_table)
-        end
-      end
+      ui = PreflightShardUI.new(my_shards)
+      ui.on_each { |shard,_| shard.rename_table(database, orig_table, copy_table) }
+      ui.run!
     end
-
   end
 end
