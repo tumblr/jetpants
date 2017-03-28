a7e84902e05013bf415beaf0dcfa1a123a61fa9a
Graham Christensen <graham@tumblr.com>
2017-03-28 09:56:00 -0400

    Replace drop_while.first with find

--- lib/jetpants/hostservice.rb
+++ lib/jetpants/hostservice.rb
@@ -21,14 +21,7 @@ module Jetpants
       throw "/proc/1/stat isn't matching, something is wrong!" if pid1_match.nil?
       pid1_name = pid1_match[1]
 
-      # We want to pick the first provider which the machine supports. Ruby 1.9.2 has no
-      # `first_where` method like
-      #    [1, 2, 3].first { |i| i > 1 } == 2
-      # but the next line fakes it, by deleting items until we find one which does match, then
-      # taking the first item in the array.
-      # This could be a `.map` or a `.select` but we really don't want to try any more than we
-      # have to.
-      provider = all_providers.drop_while { |candidate| ! candidate.preflight(host, pid1_name) }.first
+      provider = all_providers.find { |candidate| candidate.preflight(host, pid1_name) }
       raise "Cannot detect a valid service provider for #{host}" if provider.nil?
 
       return provider.new(host)
