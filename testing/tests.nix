60b34bd7e2f224e62b4465242d8230ca26e9d82a
Akshay Suryawanshi <asuryawanshi@tumblr.com>
2017-05-24 15:22:42 -0400

    Add dead master promotion test

--- testing/tests.nix
+++ testing/tests.nix
@@ -166,4 +166,27 @@ in (build-wrapper [
     ];
   })
 
+  (verify-test-case "shard-dead-master-promotion" {
+    starting-spare-dbs = 1;
+
+    test-phases = [
+      (phase "slave-clone" ''
+        (
+          echo "YES" # Confirm, cloning from standby_slave
+        ) | jetpants clone_slave --source=10.50.2.11 --target=spare
+      '')
+      (jetpants-phase "shutdown-master" ''
+        Jetpants.pool("POSTS-1-INFINITY").master.stop_mysql
+      '')
+      (phase "jetpants-promotion" ''
+        (
+          echo "YES" # Approve for promotion
+          echo "YES" # Approve after summary output. Confirmation.
+        ) | jetpants promotion --demote=10.50.2.10 --promote=10.50.2.11
+      '')
+      (assert-shard-master "POSTS-1-INFINITY" "10.50.2.11")
+      (assert-shard-slave "POSTS-1-INFINITY" "10.50.2.12")
+    ];
+  })
+
 ])
