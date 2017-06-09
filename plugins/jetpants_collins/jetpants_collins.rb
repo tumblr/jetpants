fac5788df3b3d81319f6a182ff4514358d054931
Akshay Suryawanshi <asuryawanshi@ops-graham-1cbc3221.bf2.tumblr.net>
2017-06-09 07:17:41 -0400

    refactor JetCollins#collins_set

--- plugins/jetpants_collins/jetpants_collins.rb
+++ plugins/jetpants_collins/jetpants_collins.rb
@@ -225,7 +225,78 @@ module Jetpants
       #
       # Alternatively, pass in 2 strings (field_name, value) to set just a single Collins attribute (or status)
       def collins_set(*args)
-        collins.set(*args)
+        attrs = (args.count == 1 ? args[0] : {args[0] => args[1]})
+        asset = attrs[:asset] || collins.asset.call
+
+        upcase = !attrs[:literal]
+        attrs.delete(:literal)
+
+        if asset && asset.type.downcase == 'server_node' && asset.location && asset.location.upcase != Plugin::JetCollins.datacenter
+          asset = nil unless jetcollins.inter_dc_mode?
+        end
+
+        attrs.each do |key, val|
+          val ||= ''
+          case key
+          when :asset
+            next
+          when :status
+            unless asset
+              output "WARNING: unable to set Collins status to #{val}"
+              next
+            end
+            state_val = attrs[:state]
+            previous_status = asset.status.capitalize
+            if val.include? ':'
+              raise "Attempting to set state in two places" if state_val
+              vals = val.split(':', 2)
+              val       = vals.first.capitalize
+              state_val = vals.last.upcase
+            end
+            if state_val
+              previous_state = asset.state.name.upcase
+              next unless previous_state != state_val.to_s.upcase || previous_status != val.to_s.capitalize
+                success = collins.set(asset, val, state_val)
+                unless success
+                  asset.state_create!(state_val, state_val, state_val, val)
+                  success = collins.set(asset, val, state_val)
+                end
+                raise "#{self}: Unable to set Collins state to #{state_val} and Unable to set Collins status to #{val}" unless success
+                output "Collins status:state changed from #{previous_status}:#{previous_state} to #{val.capitalize}:#{state_val.upcase}"
+            elsif previous_status != val.to_s.capitalize
+              success = collins.set(asset, val, false)
+              raise "#{self}: Unable to set Collins status to #{val}" unless success
+              output "Collins status changed from #{previous_status} to #{val}"
+            end
+          when :state
+            unless asset && asset.status && attrs[:status]
+              raise "#{self}: Unable to set state without settings a status" unless attrs[:status]
+              output "WARNING: unable to set Collins state to #{val}"
+              next
+            end
+          else
+            unless asset
+              output "WARNING: unable to set Collins attribute #{key} to #{val}"
+              next
+            end
+            previous_value = asset.send(key)
+            val = val.to_s
+            val = val.upcase if upcase
+            if previous_value != val
+              success = collins.set(asset, key.to_s.upcase, val, true)
+              raise "#{self}: Unable to set Collins attribute #{key} to #{val}" unless success
+              if (val == '' || !val) && (previous_value == '' || !previous_value)
+                false
+              elsif val == ''
+                output "Collins attribute #{key.to_s.upcase} removed (was: #{previous_value})"
+              elsif !previous_value || previous_value == ''
+                output "Collins attribute #{key.to_s.upcase} set to #{val}"
+              else
+                output "Collins attribute #{key.to_s.upcase} changed from #{previous_value} to #{val}"
+              end
+            end
+          end
+        end
       end
 
       # Returns a single downcased "status:state" string, useful when trying to compare both fields
